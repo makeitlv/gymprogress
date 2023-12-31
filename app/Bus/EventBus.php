c528d933233b2c9ad88e5302b56eb38ecd342cd7
Viktor Vipolzov <viktor.vipolzov@gmail.com>
2023-12-31 10:38:39 +0200

    Added event bus

---
+++ app/Bus/EventBus.php
@@ -0,0 +1,19 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Bus;
+
+use Illuminate\Bus\Dispatcher;
+
+class EventBus implements EventBusInterface
+{
+    public function __construct(private Dispatcher $bus)
+    {
+    }
+
+    public function dispatch(EventInterface $event): mixed
+    {
+        return $this->bus->dispatchToQueue($event);
+    }
+}
--- app/Providers/BusServiceProvider.php
+++ app/Providers/BusServiceProvider.php
@@ -6,9 +6,10 @@ namespace App\Providers;
 
 use App\Bus\CommandBus;
 use App\Bus\CommandBusInterface;
+use App\Bus\EventBus;
+use App\Bus\EventBusInterface;
 use App\Bus\QueryBus;
 use App\Bus\QueryBusInterface;
-use Fruitcake\TelescopeToolbar\Toolbar;
 use Illuminate\Bus\Dispatcher;
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Contracts\Foundation\Application;
@@ -22,9 +23,25 @@ class BusServiceProvider extends ServiceProvider
         ) {
             /** @var Dispatcher $bus */
             $bus = $app->make(Dispatcher::class);
+
             $bus->pipeThrough([]);
+            /** TODO: создать сервис который будет автоматом регестрировать команды с обработчиками */
+            $bus->map([]);
 
             return new CommandBus($bus);
+        });
+
+        $this->app->singleton(EventBusInterface::class, function (
+            Application $app,
+        ) {
+            /** @var Dispatcher $bus */
+            $bus = $app->make(Dispatcher::class);
+
+            $bus->pipeThrough([]);
+            /** TODO: создать сервис который будет автоматом регестрировать команды с обработчиками */
+            $bus->map([]);
+
+            return new EventBus($bus);
         });
 
         $this->app->singleton(QueryBusInterface::class, function (
@@ -32,6 +49,9 @@ class BusServiceProvider extends ServiceProvider
         ) {
             /** @var Dispatcher $bus */
             $bus = $app->make(Dispatcher::class);
+
+            /** TODO: создать сервис который будет автоматом регестрировать команды с обработчиками */
+            $bus->map([]);
 
             return new QueryBus($bus);
         });
